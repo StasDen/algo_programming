0caa607c6077db5dd38a204b2463f98579e0e70c
Stas Denysenko <lohopusik67@gmail.com>
2022-12-01 09:54:06 +0200

    Finished lab5

--- lab5/main.py
+++ lab5/main.py
@@ -2,8 +2,18 @@ from boyer_moore import search
 
 
 def main():
+    # Average case
     text = "ABAAABCD"
     pattern = "ABC"
+
+    # Best case: all letters in pattern and text are different. Time complexity of algorithm: O(n/m)
+    # text = "ABCDEFGH"
+    # pattern = "DEF"
+
+    # Worst case: all letters in pattern and text are the same. Time complexity of algorithm: O(n*m)
+    # text = "AAAAAAAA"
+    # pattern = "AAA"
+
     search(text, pattern)
 
 
